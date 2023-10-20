5ec1bda15e15cab3d7f1ebf9c5152a3b19bfdb21
daw <deveredev@gmail.com>
2023-10-20 19:28:40 -0400

    updated app.js

--- src/app.js
+++ src/app.js
@@ -10,3 +10,23 @@ app.get("", (req, res) => {
 app.listen(PORT, () => {
 console.log(`App up at port ${PORT}`);
 });
+
+// Test database connection stuff
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT * FROM farm;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
