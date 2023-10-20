4e7de186d676d299ee68b3c82164e4c139c6529c
daw <deveredev@gmail.com>
2023-10-20 18:41:00 -0400

    Update

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const app = require( "express" )();
+const PORT = process.env.PORT || 3000;
+app•get(""
+, (rea, res) =>
+res.send("F1 IMS (coming soon)");
+});
+app.listen(PORT, () => {
+console.log(App up at port ${PORT} );
+});
