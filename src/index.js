f8bda616b462a56bb10c04364c50eea8b99cd686
daw <deveredev@gmail.com>
2023-10-21 09:58:23 -0400

    updated index.html

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send("F1 Roasters IMS (coming soon)");
 });
 
