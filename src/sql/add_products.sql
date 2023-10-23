8fb8f638413c1ef68529726a24c07e60cb509450
daw <daw-research@protonmail.com>
2023-10-22 20:17:12 -0400

    Revert "update database products functionality"
    This reverts commit 85535e6868e75863d13a8bfe1cc5f60409f8b93c.

--- src/sql/add_products.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO product (barcode, category, supplier_id, retail, wholesale, quantity)
-VALUES (8095632, 'Light Roast', 1, 12.00, 10.00, 45),
-	   (6464642, 'Medium Roast', 1, 15.00, 12.50, 25),
-	   (9019697, 'Dark Roast', 1, 11.00, 10.00, 30),
-	   (4193677, 'Light Roast', 1, 15.00, 12.50, 18),
-	   (4133124, 'Dark Roast', 1, 13.00, 11.50, 20)
